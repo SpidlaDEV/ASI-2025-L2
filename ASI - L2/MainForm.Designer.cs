6c22e54509ede847d6eb34b3b0dda0475b47bc3d
Alex_ <Alex_@ALEX>
2025-04-27 19:58:15 -0300

    Agregar archivos de proyecto.

---
+++ ASI - L2/MainForm.Designer.cs
@@ -0,0 +1,401 @@
+﻿
+namespace ASI___L2
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.rta = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.textBox9 = new System.Windows.Forms.TextBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.button3 = new System.Windows.Forms.Button();
+			this.descuento = new System.Windows.Forms.TextBox();
+			this.totalPrecio = new System.Windows.Forms.TextBox();
+			this.precio = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel4.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.rta);
+			this.panel1.Controls.Add(this.textBox2);
+			this.panel1.Controls.Add(this.panel2);
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(12, 117);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(472, 361);
+			this.panel1.TabIndex = 0;
+			// 
+			// rta
+			// 
+			this.rta.Location = new System.Drawing.Point(101, 134);
+			this.rta.Name = "rta";
+			this.rta.Size = new System.Drawing.Size(100, 23);
+			this.rta.TabIndex = 4;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(101, 70);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 3;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(101, 44);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(113, 96);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(29, 27);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "a)";
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.panel3);
+			this.panel2.Controls.Add(this.button2);
+			this.panel2.Controls.Add(this.textBox7);
+			this.panel2.Controls.Add(this.textBox6);
+			this.panel2.Controls.Add(this.textBox5);
+			this.panel2.Controls.Add(this.textBox4);
+			this.panel2.Controls.Add(this.textBox3);
+			this.panel2.Location = new System.Drawing.Point(3, 70);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(472, 361);
+			this.panel2.TabIndex = 5;
+			// 
+			// panel4
+			// 
+			this.panel4.Controls.Add(this.textBox9);
+			this.panel4.Controls.Add(this.button4);
+			this.panel4.Controls.Add(this.textBox8);
+			this.panel4.Controls.Add(this.label4);
+			this.panel4.Controls.Add(this.label3);
+			this.panel4.Location = new System.Drawing.Point(52, 41);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(463, 358);
+			this.panel4.TabIndex = 6;
+			// 
+			// textBox9
+			// 
+			this.textBox9.Location = new System.Drawing.Point(52, 122);
+			this.textBox9.Name = "textBox9";
+			this.textBox9.Size = new System.Drawing.Size(100, 20);
+			this.textBox9.TabIndex = 4;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(199, 75);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(54, 75);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(100, 20);
+			this.textBox8.TabIndex = 2;
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(52, 32);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 1;
+			this.label4.Text = "g)";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(76, 32);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 0;
+			// 
+			// panel3
+			// 
+			this.panel3.Controls.Add(this.panel4);
+			this.panel3.Controls.Add(this.button3);
+			this.panel3.Controls.Add(this.descuento);
+			this.panel3.Controls.Add(this.totalPrecio);
+			this.panel3.Controls.Add(this.precio);
+			this.panel3.Controls.Add(this.label2);
+			this.panel3.Location = new System.Drawing.Point(26, 90);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(466, 371);
+			this.panel3.TabIndex = 6;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(101, 137);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// descuento
+			// 
+			this.descuento.Location = new System.Drawing.Point(244, 111);
+			this.descuento.Name = "descuento";
+			this.descuento.Size = new System.Drawing.Size(100, 20);
+			this.descuento.TabIndex = 3;
+			// 
+			// totalPrecio
+			// 
+			this.totalPrecio.Location = new System.Drawing.Point(101, 166);
+			this.totalPrecio.Name = "totalPrecio";
+			this.totalPrecio.Size = new System.Drawing.Size(100, 20);
+			this.totalPrecio.TabIndex = 2;
+			// 
+			// precio
+			// 
+			this.precio.Location = new System.Drawing.Point(101, 111);
+			this.precio.Name = "precio";
+			this.precio.Size = new System.Drawing.Size(100, 20);
+			this.precio.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(52, 61);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "c)";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(70, 143);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(60, 168);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(100, 20);
+			this.textBox7.TabIndex = 4;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(60, 117);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(100, 20);
+			this.textBox6.TabIndex = 3;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(60, 90);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 2;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(60, 63);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 1;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(60, 38);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 0;
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(15, 3);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 6;
+			this.button5.Text = "panel a";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.Button5Click);
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(103, 3);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(75, 23);
+			this.button6.TabIndex = 7;
+			this.button6.Text = "panel b";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.Button6Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.radioButton2);
+			this.groupBox1.Controls.Add(this.radioButton1);
+			this.groupBox1.Location = new System.Drawing.Point(211, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(200, 100);
+			this.groupBox1.TabIndex = 8;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "groupBox1";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.Location = new System.Drawing.Point(6, 19);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(104, 24);
+			this.radioButton1.TabIndex = 0;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "radioButton1";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.Location = new System.Drawing.Point(6, 49);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(104, 24);
+			this.radioButton2.TabIndex = 1;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "radioButton2";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.Location = new System.Drawing.Point(41, 32);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(104, 24);
+			this.radioButton3.TabIndex = 9;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "radioButton3";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.ClientSize = new System.Drawing.Size(496, 385);
+			this.Controls.Add(this.radioButton3);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.panel1);
+			this.Name = "MainForm";
+			this.Text = "Hola!";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel4.ResumeLayout(false);
+			this.panel4.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			this.groupBox1.ResumeLayout(false);
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.TextBox textBox9;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.TextBox precio;
+		private System.Windows.Forms.TextBox totalPrecio;
+		private System.Windows.Forms.TextBox descuento;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label rta;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
---
+++ ASI - L2/MainForm.cs
@@ -0,0 +1,144 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ASI___L2
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			
+			panel1.Hide();
+			panel2.Hide();
+			panel3.Hide();
+			panel4.Hide();
+		}
+		
+		int toInt(string n)
+		{
+			return int.Parse(n);
+		}
+		
+		double toDouble(string n)
+		{
+			return Convert.ToDouble(n);
+		}
+		
+		// a. Sumar dos valores enteros y mostrar el resultado.
+		//	¿Qué cambios haría para sumar valores reales?
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text)) {
+				MessageBox.Show("Sin datos", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+				return;
+			}
+			
+			// bool isValid = !int.TryParse(textBox1.Text, out int resultado_1);
+			
+			// if (isValid) {
+			//	MessageBox.Show("Necesito un número", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+			//	return;
+			// }
+			
+			//if (!int.TryParse(textBox2.Text, out int resultado_2)) {
+				//MessageBox.Show("Necesito un número 2", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+				//return;
+			//}
+			
+			rta.Text = (toInt(textBox1.Text) + toInt(textBox2.Text)).ToString();
+		}
+		
+		
+		// b. Obtener el promedio de 4 valores reales y mostrarlos por pantalla.	
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			double a = toDouble(textBox3.Text);
+			double b = toDouble(textBox4.Text);
+			double c = toDouble(textBox5.Text);
+			double d = toDouble(textBox6.Text);
+			
+			// double rta = (a + b + c + d) / 4;
+			// string rta2 = rta.ToString();
+				
+			textBox7.Text = ((a + b + c + d) / 4).ToString();
+		}
+		
+		// c. Calcular el precio final de un producto, sabiendo que si el precio inicial es mayor a $50 mil se hará un descuento de 25%, de lo contrario no se aplica descuento.
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+			double _precio = toDouble(precio.Text);
+			double _descuento = toDouble(descuento.Text);
+			
+			double _total = _precio;
+			
+			if (_precio > 50000) {
+				double _subtotal = _precio * (_descuento / 100);
+				_total = _precio - _subtotal;
+			}
+			
+			totalPrecio.Text = _total.ToString();
+			// totalPrecio.Text = (_precio > 50000 ? _precio * 0.75 : _precio).ToString();
+		}
+		
+		// d. Obtener el mes correspondiente al ingresar un número entero entre 1 y 12. Si se ingresa un valor fuera de este rango debe indicar un error.
+		// DateTime		
+		// e. Obtener las raíces de una ecuación cuadrática, incluya la posibilidad de obtener raíces complejas.  
+		// f. Indicar cuál de n valores enteros ingresados es el mayor y cuál es el menor. 
+		// g. Obtener la suma de n números reales. 
+		
+		List<double> numeros = new List<double>();
+		void Button43Click(object sender, EventArgs e)
+		{
+			double aux = toDouble(textBox8.Text);
+			numeros.Add(aux);
+			
+			// double total = 0;
+			//for (int i = 0; i < numeros.Count; i++) {
+			//	total = total + numeros[i];
+			// }
+			
+			textBox9.Text = numeros.Sum().ToString();
+		}
+		
+		// h. Obtener el promedio de n valores ingresados.
+		int index = 0;
+		void Button4Click(object sender, EventArgs e)
+		{
+			index = index + 1;
+			double aux = toDouble(textBox8.Text);
+			numeros.Add(aux);
+			
+			double total = numeros.Sum() / index;
+			
+			textBox9.Text = total.ToString();
+		}
+		
+		void Button5Click(object sender, EventArgs e)
+		{
+			panel1.Show();
+			panel2.Hide();
+			panel3.Hide();
+			panel4.Hide();
+		}
+		
+		void Button6Click(object sender, EventArgs e)
+		{
+			panel1.Hide();
+			panel2.Show();
+			panel3.Hide();
+			panel4.Hide();
+		}
+	}
+		
+}
